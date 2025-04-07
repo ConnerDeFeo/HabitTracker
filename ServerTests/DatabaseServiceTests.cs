9cabc309c5849de701a8ca4525cbc3138b740fb7
ConnerDeFeo <ninjanerozz@gmail.com>
2025-04-06 20:58:04 -0400

    setting up tests

---
+++ ServerTests/DatabaseServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Mongo2Go;
+using MongoDB.Driver;
+using Xunit;
+
+public class MongoDbServiceTests
+{
+    private MongoDbRunner _mongoDbRunner;
+
+    public MongoDbServiceTests()
+    {
+        _mongoDbRunner = MongoDbRunner.Start();
+    }
+
+    [Fact]
+    public void TestDatabaseCreation()
+    {
+
+        var mongoService = new MongoDbService(_mongoDbRunner.ConnectionString, "TestDb");
+        var collection = mongoService.GetCollection<BsonDocument>("TestCollection");
+
+        // Perform operations on the collection (insert, find, etc.)
+        collection.InsertOne(new BsonDocument { { "Name", "Test" } });
+        var result = collection.Find(new BsonDocument()).FirstOrDefault();
+
+        Assert.NotNull(result);
+        Assert.Equal("Test", result["Name"].AsString);
+    }
+
+    public void Dispose()
+    {
+        _mongoDbRunner.Dispose();
+    }
+}
