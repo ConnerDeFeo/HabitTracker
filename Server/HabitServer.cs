7ed64f84a2b71a2e23b743370dd665c0b258f50c
ConnerDeFeo <ninjanerozz@gmail.com>
2025-04-06 21:53:38 -0400

    restarting

---
+++ Server/HabitServer.cs
@@ -0,0 +1,16 @@
+public class HabitServer
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        app.UseAuthorization();
+        app.MapControllers();
+
+        app.Run();
+    }
+}
--- Server/Services/MongoDbService.cs
+++ Server/Services/MongoDbService.cs
@@ -1,17 +1,30 @@
 using MongoDB.Driver;
 
-public class MongoDbService
-{
-    private readonly IMongoDatabase _database;
-
-    public MongoDbService(IConfiguration config)
+    public class MongoDbService
     {
-        var client = new MongoClient(config.GetConnectionString("MongoDb"));
-        _database = client.GetDatabase(config["HabitTracker"]);
-    }
+        private readonly IMongoDatabase _database;
 
-    public IMongoCollection<T> GetCollection<T>(string name)
-    {
-        return _database.GetCollection<T>(name);
+        public MongoDbService(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("MongoDb"));
+            _database = client.GetDatabase(config["HabitTracker"]);
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string name)
+        {
+            return _database.GetCollection<T>(name);
+        }
+
+        public void InsertDocument<T>(string collectionName, T document)
+        {
+            var collection = GetCollection<T>(collectionName);
+            collection.InsertOne(document); 
+        }
+
+        // Optionally, you can also add a method to insert multiple documents
+        public void InsertDocuments<T>(string collectionName, IEnumerable<T> documents)
+        {
+            var collection = GetCollection<T>(collectionName);
+            collection.InsertMany(documents);
+        }
     }
-}
