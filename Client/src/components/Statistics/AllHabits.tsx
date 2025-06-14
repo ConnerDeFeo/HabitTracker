48d7ef61bca76e67b9c0fa66288f326e036318fa
ConnerDeFeo <ninjanerozz@gmail.com>
2025-06-13 20:12:45 -0400

    all that remains is the most complex component

--- Client/src/components/Statistics/AllHabits.tsx
+++ Client/src/components/Statistics/AllHabits.tsx
@@ -12,16 +12,35 @@ const AllHabits = (
 )=>{
     const {activeHabits, nonActiveHabits,handleHabitSelection,selectedHabitId,smallScreen} = props;
     const habitNameStyling = "my-7 cursor-pointer"; //Styling for each of the habit names
-    return(
+    return smallScreen ? 
+        <>
+            <select className="habitBorder mb-10 w-40 text-center text-xs pl-1"
+                onChange={(e)=>handleHabitSelection(e.target.value)}
+            >
+                {activeHabits.map(habit=>
+                    <option
+                        key={habit.id}
+                        value={habit.id}
+                    >{habit.name}</option>
+                )}
+                {nonActiveHabits.map(habit=>
+                    <option
+                        key={habit.id}
+                        value={habit.id}
+                    >{habit.name}</option>
+                )}
+            </select>
+        </>
+        :
         <div className="habitBorder w-75 overflow-y-auto text-4xl text-center">
-            {activeHabits.map((habit)=>
+            {activeHabits.map(habit=>
                 <p 
                     key={habit.name} 
                     className={`${habitNameStyling} ${(selectedHabitId===habit.id! && "line-through")}`}
                     onClick={()=>handleHabitSelection(habit.id!)}
                 >{habit.name}</p>
             )}
-            {nonActiveHabits.map((habit)=>
+            {nonActiveHabits.map(habit=>
                 <p 
                     key={habit.name} 
                     className={`${habitNameStyling} ${(selectedHabitId===habit.id! && "line-through")}`}
@@ -29,7 +48,7 @@ const AllHabits = (
                 >{habit.name}</p>
             )}
         </div>
-    );
+    ;
 }
 
 export default AllHabits;
--- Client/src/components/Statistics/HabitData.tsx
+++ Client/src/components/Statistics/HabitData.tsx
@@ -3,24 +3,21 @@ import HistoricalData from "../../types/HistoricalData";
 //Habit data shown regarding streaks and days completed in the statistics page
 const HabitData = (props:{historicalData?: HistoricalData})=>{
     const {historicalData} = props;
+    const textSizing:string = "text-xl md:text-2xl lg:text-3xl xl:text-4xl";
     return (
         <div className="h-25 mb-5">
             {historicalData === undefined ? 
                 <p className="text-6xl text-center">No habits to view!</p>
                 :
-                <div className="flex justify-between w-[90%] mx-auto">
-                    <div>
-                        <p className="text-4xl mb-5">{historicalData.habit.name}</p>
-                        <p className="text-4xl">
-                            {`${historicalData.totalValueCompleted} 
-                            ${historicalData.habit.valueUnitType || "days completed"} 
-                            sense ${historicalData.habit.dateCreated}`}
-                        </p>
-                    </div>
-                    <div>
-                        <p className="text-4xl mb-5">Longest Streak: {historicalData.longestStreak}</p>
-                        <p className="text-4xl">Current Streak: {historicalData.currentStreak}</p>
-                    </div>
+                <div className="grid md:grid-cols-3 justify-between md:w-[90%] MD:mx-auto">
+                    <p className={`${textSizing} mb-5 md:row-start-1 md:col-start-1`}>{historicalData.habit.name}</p>
+                    <p className={`${textSizing} mb-5 md:row-start-1 md:col-start-3 md:text-right`}>Longest Streak: {historicalData.longestStreak}</p>
+                    <p className={`${textSizing} md:row-start-2 md:col-start-3 md:text-right`}>Current Streak: {historicalData.currentStreak}</p>
+                    <p className={`${textSizing} text-xl md:text-2xl lg:text-3xl md:row-start-2 md:col-start-1 md:col-span-2`}>
+                        {`${historicalData.totalValueCompleted} 
+                        ${historicalData.habit.valueUnitType || "days completed"} 
+                        sense ${historicalData.habit.dateCreated}`}
+                    </p>
                 </div>
             }
         </div>
