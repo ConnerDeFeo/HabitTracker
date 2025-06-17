1cac71c6590ad548924c39951f049afca76086c1
ConnerDeFeo <ninjanerozz@gmail.com>
2025-06-17 12:27:38 -0400

    so many little mistakes

--- Server/platform/hooks/postdeploy/00_get_certificate.sh
+++ Server/platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,1 +1,1 @@
-sudo certbot -n -d http://habittrackerapi-env.eba-6npmzimh.us-east-2.elasticbeanstalk.com/ --nginx --agree-tos --email ninjanerozz@gmail.com
+sudo certbot -n -d habittrackerapi-env.eba-6npmzimh.us-east-2.elasticbeanstalk.com --nginx --agree-tos --email ninjanerozz@gmail.com
