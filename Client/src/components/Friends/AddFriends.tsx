eb916fa38eb1519ef65bc86ce4335f845c0069fc
ConnerDeFeo <ninjanerozz@gmail.com>
2025-07-07 19:26:43 -0400

    enter now works with this lets go

--- Client/src/components/Friends/AddFriends.tsx
+++ Client/src/components/Friends/AddFriends.tsx
@@ -21,16 +21,23 @@ const AddFriend = (
         } 
     }
 
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') 
+            fetchUsers();
+    };
+
     return(
         <>
-            <img src="BasicArrow.png" alt="basic arrow" className="rotate-180 h-8 w-8 ml-[7.5%] my-5 cursor-pointer" onClick={()=>setAddFriends(false)}/>
-            <div className="relative text-xl h-10 w-80 habitBorder mx-auto flex">
-                <input value={searchPhrase} onChange={(e)=>setSearchPhrase(e.target.value)} className="focus:outline-none pl-3"/>
-                <img className="absolute h-6 w-6 right-3 top-[0.3rem] cursor-pointer" src="Search.png" alt="search" onClick={fetchUsers}/>
+            <div className="w-70 sm:w-100 md:w-125 mx-auto">
+                <img src="BasicArrow.png" alt="basic arrow" className="rotate-180 h-8 w-8 my-5 cursor-pointer" onClick={()=>setAddFriends(false)}/>
+                <div className="relative text-xl h-10 habitBorder mx-auto flex">
+                    <input value={searchPhrase} onChange={(e)=>setSearchPhrase(e.target.value)} className="focus:outline-none pl-3" onKeyDown={handleKeyDown}/>
+                    <img className="absolute h-6 w-6 right-3 top-[0.3rem] cursor-pointer" src="Search.png" alt="search" onClick={fetchUsers}/>
+                </div>
             </div>
             <div className="grid gap-y-10 py-10">
                 { Object.entries(displayedUsers).map(([key, value]) =>
-                    <div key={key} className="habitBorder w-[85%] mx-auto flex justify-between items-center mx-auto px-[5%] h-30">
+                    <div key={key} className="habitBorder w-[85%] max-w-125 mx-auto flex justify-between items-center mx-auto px-[5%] h-30">
                         {value ? 
                             <img src={`https://habit-tracker-photos.s3.amazonaws.com/${value}`} alt={`${key} pfp`} className={pfpSizing}/>
                             :
