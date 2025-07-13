a2e154e5cdc58bbe7c988656dc92fd46038ce112
ConnerDeFeo <ninjanerozz@gmail.com>
2025-07-12 22:19:27 -0400

    final version for now, may add an AI chat bot

---
+++ Server/publish/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo certbot -n -d habittrackerapi-env.eba-6npmzimh.us-east-2.elasticbeanstalk.com --nginx --agree-tos --email ninjanerozz@gmail.com
--- Server/publish/hooks/postdeploy/00_get_certificate.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-sudo certbot -n -d habittrackerapi-env.eba-6npmzimh.us-east-2.elasticbeanstalk.com --nginx --agree-tos --email ninjanerozz@gmail.com
